275d0d52cd05663ebb45ae4966740b2032c1df58
Omar Khalili <OmarKhalili810@gmail.com>
2025-03-13 13:57:30 +0200

    adding the flight class extensions method

--- Airport Ticket Booking.Test/obj/Debug/net9.0/Airport Ticket Booking.Test.AssemblyInfo.cs
+++ Airport Ticket Booking.Test/obj/Debug/net9.0/Airport Ticket Booking.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Airport Ticket Booking.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58704db25c6b7f1fa4d3dd3217e61bf69a2db0c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa926b8908a3331b98dbaf9b51a081a82fc9cf0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Airport Ticket Booking.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Airport Ticket Booking.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
